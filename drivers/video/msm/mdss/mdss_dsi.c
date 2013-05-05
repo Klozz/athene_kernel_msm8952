d3107a78cd995d115c234622bfdfb5c0f1b35ed6
Se Hun Kim <sean2.kim@lge.com>
2013-05-05 15:33:46 -0700

    msm: mdss: Adding lcd notifier
    Supports notifier for start&end of LCD power on&off
    
    Change-Id: Idaa685a2c554286da5f764519305d830c4d1a1bd
    Signed-off-by: Se Hun Kim <sean2.kim@lge.com>
    Signed-off-by: Jongrak Kwon <jongrak.kwon@lge.com>
    Signed-off-by: Devin Kim <dojip.kim@lge.com>
    Signed-off-by: Zachariah <zkennedy87@gmail.com>
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>
    
    Conflicts:
    	drivers/video/msm/mdss/Makefile
    	drivers/video/msm/mdss/mdss_dsi.c
    
    Conflicts:
    	drivers/video/msm/mdss/Makefile
    
    Conflicts:
    	drivers/video/msm/mdss/Makefile
    	drivers/video/msm/mdss/mdss_dsi.c
    
    Conflicts:
    	drivers/video/msm/mdss/Makefile
    	drivers/video/msm/mdss/mdss_dsi.c
    
    Conflicts:
    	drivers/video/msm/mdss/Makefile
    
    Conflicts:
    	drivers/video/msm/mdss/mdss_dsi.c
    
    Conflicts:
    	drivers/video/msm/mdss/mdss_dsi.c
    
    Conflicts:
    	drivers/video/msm/mdss/mdss_dsi.c
    
    Conflicts:
    	drivers/video/msm/mdss/Makefile
    	drivers/video/msm/mdss/mdss_dsi.c

--- drivers/video/msm/mdss/mdss_dsi.c
+++ drivers/video/msm/mdss/mdss_dsi.c
@@ -25,6 +25,7 @@
 #include <linux/clk.h>
 #include <linux/uaccess.h>
 #include <linux/pm_qos.h>
+#include <linux/lcd_notify.h>
 
 #include "mdss.h"
 #include "mdss_panel.h"
@@ -2215,6 +2216,7 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 							pdata);
 		break;
 	case MDSS_EVENT_UNBLANK:
+		lcd_notifier_call_chain(LCD_EVENT_ON_START, NULL);
 		if (ctrl_pdata->on_cmds.link_state == DSI_LP_MODE)
 			rc = mdss_dsi_unblank(pdata);
 		break;
@@ -2227,9 +2229,11 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 			ctrl_pdata->bl_on_defer(ctrl_pdata);
 		if (ctrl_pdata->on_cmds.link_state == DSI_HS_MODE)
 			rc = mdss_dsi_unblank(pdata);
+		lcd_notifier_call_chain(LCD_EVENT_ON_END, NULL);
 		pdata->panel_info.esd_rdy = true;
 		break;
 	case MDSS_EVENT_BLANK:
+		lcd_notifier_call_chain(LCD_EVENT_OFF_START, NULL);
 		power_state = (int) (unsigned long) arg;
 		if (ctrl_pdata->off_cmds.link_state == DSI_HS_MODE)
 			rc = mdss_dsi_blank(pdata, power_state);
@@ -2240,6 +2244,7 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 		if (ctrl_pdata->off_cmds.link_state == DSI_LP_MODE)
 			rc = mdss_dsi_blank(pdata, power_state);
 		rc = mdss_dsi_off(pdata, power_state);
+		lcd_notifier_call_chain(LCD_EVENT_OFF_END, NULL);
 		break;
 	case MDSS_EVENT_CONT_SPLASH_FINISH:
 		if (ctrl_pdata->off_cmds.link_state == DSI_LP_MODE)
